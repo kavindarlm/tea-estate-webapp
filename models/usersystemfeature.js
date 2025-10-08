3d403caa5d621ee66bbd28dcd26d7c0812062710
kavindarlm <kavindarlm.21@uom.lk>
2025-10-08 16:58:43 +0530

    refactor: Remove unused helper method comments from model association definitions

--- models/usersystemfeature.js
+++ models/usersystemfeature.js
@@ -4,11 +4,6 @@ const {
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class UserSystemFeature extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       // Belongs to User
       UserSystemFeature.belongsTo(models.User, {
